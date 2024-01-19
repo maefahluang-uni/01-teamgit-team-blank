a62ddcf4c6be3efa6904684f359011a0b2973705
NickMFU <97875375+NickMFU@users.noreply.github.com>
2024-01-19 09:04:16 +0000

    master fix

--- src/main/java/util/Counter.java
+++ src/main/java/util/Counter.java
@@ -2,63 +2,58 @@ package util;
 
 import java.util.StringTokenizer;
 
-//This is an example (faulty) class to be used for A4 Task1 and Task2
 public class Counter {
-	public int _ctr;
-	public int _multiplier;
+    public int _ctr;
+    public int _multiplier;
 
-	// method increments _ctr
-	public void increment() {
-		_ctr++; // faulty increment
-	}
+    // method increments _ctr
+    public void increment() {
+        _ctr++;
+    }
 
-	// method decrements _ctr
-	public void decrement() {
-		_ctr++; // faulty decrement
-	}
+    // method decrements _ctr
+    public void decrement() {
+        _ctr--;
+    }
 
-	// method resets _ctr
-	public void reset() {
-		_ctr = 1; // faulty reset, _ctr should rather be 0
-	}
+    // method resets _ctr
+    public void reset() {
+        _ctr = 0;
+    }
 
-	// method multiplies _ctr by n
-	public void multiplyBy(int multiplier) {
-		_multiplier = multiplier; // no fault here
-		_ctr = _ctr * _multiplier; // no fault here
-	}
+    // method multiplies _ctr by n
+    public void multiplyBy(int multiplier) {
+        _multiplier = multiplier;
+        _ctr = _ctr * _multiplier;
+    }
 
-	// TODO: dev1- method for increment to closest even number
-	public void incrementToEven() {
-		if (_ctr % 2 != 0) {
-            _ctr++;
-        }
-	}
+    // method for increment to closest even number
+    public void incrementToEven() {
+        _ctr += (_ctr % 2 == 0) ? 2 : 1;
+    }
 
-	// TODO: dev1- method for decrement to closest even number
-	public void decrementToEven() {
-		if (_ctr % 2 != 0) {
-            _ctr--;
-        }
+    // method for decrement to closest even number
+    public void decrementToEven() {
+        _ctr -= (_ctr % 2 == 0) ? 2 : 1;
+    }
 
-	// TODO: dev2- method for increment to closest prime number
-	public void incrementToPrime() {
-		_ctr = getNextPrime(_ctr);
-	}
+    // method for increment to closest prime number
+    public void incrementToPrime() {
+        _ctr = getNextPrime(_ctr);
+    }
 
-	// TODO: dev2- method for decrement to closest prime number
-	public void decrementToPrime() {
-		_ctr = getPreviousPrime(_ctr);
-	}
+    // method for decrement to closest prime number
+    public void decrementToPrime() {
+        _ctr = getPreviousPrime(_ctr);
+    }
 
-	// TODO: dev3- count the frequency of word in sentence,
-	// refactor source code from dev1 and dev2
-	 public void countFrequency(String word, String sentence) {
+    // count the frequency of word in sentence
+    public void countFrequency(String word, String sentence) {
         StringTokenizer tokenizer = new StringTokenizer(sentence);
         int frequency = 0;
 
         while (tokenizer.hasMoreTokens()) {
-            if (tokenizer.nextToken().equalsIgnoreCase(word)) {
+            if (tokenizer.nextToken().equals(word)) {
                 frequency++;
             }
         }
@@ -68,7 +63,7 @@ public class Counter {
 
     // Helper method to check if a number is prime
     private boolean isPrime(int num) {
-        if (num <= 1) {
+        if (num < 2) {
             return false;
         }
         for (int i = 2; i <= Math.sqrt(num); i++) {
@@ -81,20 +76,19 @@ public class Counter {
 
     // Helper method to get the next prime number
     private int getNextPrime(int num) {
-        int nextNum = num + 1;
-        while (!isPrime(nextNum)) {
-            nextNum++;
+        int next = num + 1;
+        while (!isPrime(next)) {
+            next++;
         }
-        return nextNum;
+        return next;
     }
 
     // Helper method to get the previous prime number
     private int getPreviousPrime(int num) {
-        int prevNum = num - 1;
-        while (!isPrime(prevNum)) {
-            prevNum--;
+        int previous = num - 1;
+        while (!isPrime(previous)) {
+            previous--;
         }
-        return prevNum;
+        return previous;
     }
-
 }
