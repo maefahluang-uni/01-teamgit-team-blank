b8a426fa78ca9166e26aa1b3fd46cc47c06f7529
NickMFU <97875375+NickMFU@users.noreply.github.com>
2024-01-19 08:57:28 +0000

    dev1 dix

--- src/main/java/util/Counter.java
+++ src/main/java/util/Counter.java
@@ -14,12 +14,12 @@ public class Counter {
 
 	// method decrements _ctr
 	public void decrement() {
-		_ctr--; // faulty decrement
+		_ctr++; // faulty decrement
 	}
 
 	// method resets _ctr
 	public void reset() {
-		_ctr = 0; // faulty reset, _ctr should rather be 0
+		_ctr = 1; // faulty reset, _ctr should rather be 0
 	}
 
 	// method multiplies _ctr by n
@@ -30,74 +30,31 @@ public class Counter {
 
 	// TODO: dev1- method for increment to closest even number
 	public void incrementToEven() {
-		 if (_ctr % 2 != 0) {
+		if (_ctr % 2 != 0) {
             _ctr++;
         }
 	}
 
 	// TODO: dev1- method for decrement to closest even number
 	public void decrementToEven() {
-		 if (_ctr % 2 != 0) {
+		if (_ctr % 2 != 0) {
             _ctr--;
         }
+
+	// TODO: dev2- method for increment to closest prime number
+	public void incrementToPrime() {
+		_ctr = -99;
 	}
 
-	
-   // method for increment to closest prime number
-    public void incrementToPrime() {
-        _ctr = getNextPrime(_ctr);
-    }
+	// TODO: dev2- method for decrement to closest prime number
+	public void decrementToPrime() {
+		_ctr = -99;
+	}
 
-    // method for decrement to closest prime number
-    public void decrementToPrime() {
-        _ctr = getPreviousPrime(_ctr);
-    }
+	// TODO: dev3- count the frequency of word in sentence,
+	// refactor source code from dev1 and dev2
+	public void countFrequency(String word, String sentence) {
+		_ctr = -99;
+	}
 
-    // count the frequency of word in sentence
-    public void countFrequency(String word, String sentence) {
-        StringTokenizer tokenizer = new StringTokenizer(sentence);
-        int frequency = 0;
-
-        while (tokenizer.hasMoreTokens()) {
-            if (tokenizer.nextToken().equalsIgnoreCase(word)) {
-                frequency++;
-            }
-        }
-
-        _ctr = frequency;
-    }
-
-    // Helper method to check if a number is prime
-    private boolean isPrime(int num) {
-        if (num <= 1) {
-            return false;
-        }
-        for (int i = 2; i <= Math.sqrt(num); i++) {
-            if (num % i == 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    // Helper method to get the next prime number
-    private int getNextPrime(int num) {
-        int nextNum = num + 1;
-        while (!isPrime(nextNum)) {
-            nextNum++;
-        }
-        return nextNum;
-    }
-
-    // Helper method to get the previous prime number
-    private int getPreviousPrime(int num) {
-        int prevNum = num - 1;
-        while (!isPrime(prevNum)) {
-            prevNum--;
-        }
-        return prevNum;
-    }
 }
-	
-
-
